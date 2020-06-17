9d32daae0d2aa4546baa567e5970ad7761330d1a
wittySana89 <67058336+wittySana89@users.noreply.github.com>
2020-06-17 23:38:15 +1000

    Add files via upload

---
+++ AddButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddButtons : MonoBehaviour {
+
+    [SerializeField]
+    private Transform memoryField;
+
+    [SerializeField]
+    private GameObject btn;
+
+    
+
+    void Awake()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(memoryField,false);
+        }
+
+    }
+	
+	}
+
---
+++ GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public List<Button> btns = new List<Button>();
+
+    void Awake()
+    { 
+    
+    }
+
+    void getButtons()
+    { 
+    
+    }
+}
